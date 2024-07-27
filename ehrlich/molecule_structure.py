f1ab837686e6a54ef8a078b412ac1512a5c11119
hlebnoe_pole <glebpavlyuk@gmail.com>
2024-07-27 19:00:10 +0300

    refactor project method to matrices

--- ehrlich/molecule_structure.py
+++ ehrlich/molecule_structure.py
@@ -101,10 +101,5 @@ class MoleculeStructure(Molecule, Mesh):
         Finds closest atom to each mesh vertex and writes to vamap field
         """
 
-        get_dist = lambda x, y: np.linalg.norm(x - y)
-        self.vamap = [None] * len(self.vcoords)
-        for point_idx, coords1 in enumerate(self.vcoords):
-            dists = {idx: get_dist(coords2, coords1) for idx, coords2 in enumerate(self.acoords) if idx != point_idx}
-            dists = {k: v for k, v in sorted(dists.items(), key=lambda item: item[1])}
-            best_atom_idx = list(dists.keys())[0]
-            self.vamap[point_idx] = best_atom_idx
+        d = np.linalg.norm((self.vcoords[:, np.newaxis, :] - self.acoords), axis=2)
+        self.vamap = np.argmin(d, axis=1)
